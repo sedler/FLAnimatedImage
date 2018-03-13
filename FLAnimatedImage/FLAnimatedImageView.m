8e1ce3593942b920b574b6b30ef18563a3962e21
Stefan Edler <sedler@know-center.at>
2018-03-13 11:21:20 +0100

    Update FLAnimatedImageView.m

--- FLAnimatedImage/FLAnimatedImageView.m
+++ FLAnimatedImage/FLAnimatedImageView.m
@@ -301,7 +301,7 @@ static NSUInteger gcd(NSUInteger a, NSUInteger b)
         // Note: The display link's `.frameInterval` value of 1 (default) means getting callbacks at the refresh rate of the display (~60Hz).
         // Setting it to 2 divides the frame rate by 2 and hence calls back at every other display refresh.
         const NSTimeInterval kDisplayRefreshRate = 60.0; // 60Hz
-        self.displayLink.frameInterval = MAX([self frameDelayGreatestCommonDivisor] * kDisplayRefreshRate, 1);
+        self.displayLink.frameInterval = MAX([self frameDelayGreatestCommonDivisor] * kDisplayRefreshRate, 2);
 
         self.displayLink.paused = NO;
     } else {
